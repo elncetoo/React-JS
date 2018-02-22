f9314573b4355de8c258a08cb0a129bca23fc176
elincetoo <svilena_koleva@yahoo.com>
2018-02-22 11:45:06 +0100

    babel generated JS

---
+++ to-do-app/public/scripts/app.js
@@ -0,0 +1,17 @@
+console.log('App.js is running!');
+
+//JSX - JavaScript XML
+//var template =  <p>This is JSX from App.js!</p>;
+
+var template = React.createElement(
+    "h1",
+    { id: "someid" }, //will be null if no id defined
+    "This is sample txt."
+  ); 
+  //babel generated JS from>> var template = <h1 id="someid">This is shit!</h1>
+ 
+  
+var appRoot = document.getElementById('app');
+
+
+ReactDOM.render(template, appRoot);
