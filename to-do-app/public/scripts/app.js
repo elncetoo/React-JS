4e065757c362ca9b8f49c12b361b124a985ff0ed
elincetoo <svilena_koleva@yahoo.com>
2018-02-23 18:19:05 +0100

    exploring JSX /10

--- to-do-app/public/scripts/app.js
+++ to-do-app/public/scripts/app.js
@@ -4,11 +4,38 @@ console.log('App.js is running!');
 
 //JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'To-Do App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'To-Do App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some text'
+    )
 );
-
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Svilena Koleva'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 31'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Copenhagen'
+    )
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- to-do-app/src/app.js
+++ to-do-app/src/app.js
@@ -1,9 +1,19 @@
 console.log('App.js is running!');
 
 //JSX - JavaScript XML
-var template =  <h1>To-Do App</h1>;
-
+var template = ( 
+    <div>
+        <h1>To-Do App</h1>  
+        <p>This is some text</p>
+    </div>
+);
+var templateTwo = (
+    <div>
+        <h1>Svilena Koleva</h1>
+        <p>Age: 31</p>
+        <p>Location: Copenhagen</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
-
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
